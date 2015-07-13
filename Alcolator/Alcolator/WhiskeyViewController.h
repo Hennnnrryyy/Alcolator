cdabde317f81cc1ae85a969f6dda79a711a440e6
Henry <henrysmithjr@me.com>
2015-07-13 01:28:13 -0400

    Checkpoint 20 completed, implementing calculations

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Henry Smith on 7/13/15.
+//  Copyright (c) 2015 Henry Smith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
