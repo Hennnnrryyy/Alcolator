afbea642952fee94257ec5750412bb61300e2b45
Henry <henrysmithjr@me.com>
2015-07-13 23:20:04 -0400

    Checkpoint 21 completed, basic navigation

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Henry Smith on 7/13/15.
+//  Copyright (c) 2015 Henry Smith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
